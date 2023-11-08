6ccab8d86b39cf87b211233338bc2babef4aec2f
0xAr0k <arok.eth@proton.me>
2023-11-08 11:22:55 +0700

    updates

--- lua/arok/plugins/neo-tree.lua
+++ lua/arok/plugins/neo-tree.lua
@@ -6,11 +6,11 @@ end
 -- recommended settings from nvim-neo-tree documentation
 
 neotree.setup({
-    filesystem = {
-    filtered_items = {
-      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-      hide_dotfiles = false,
-      hide_gitignored = true,
-    },
-    }
+	filesystem = {
+		filtered_items = {
+			visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+			hide_dotfiles = false,
+			hide_gitignored = true,
+		},
+	},
 })
